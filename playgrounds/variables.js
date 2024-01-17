4e7bfbb884068152b2d853abad5e64b3cb121a84
ValyaDudchak <48631216+ValyaDudchak@users.noreply.github.com>
2024-01-17 23:17:33 +0200

    task 3: variables

---
+++ playgrounds/variables.js
@@ -0,0 +1,28 @@
+// 1:
+
+let name;
+let admin;
+
+name = "Іван";
+admin = name;
+
+// console.log(admin);
+// alert(admin);
+
+// 2:
+
+let planetEarth;
+let currentVisitorName;
+
+//  3:
+const BIRTHDAY = "25.03.2005";
+const age = "someCode(BIRTHDAY)"
+
+// 
+
+let test1 = "test2";
+if(true) {
+    let test1 = "test1";
+}
+
+alert(test1);
