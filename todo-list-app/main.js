d036660c699d5bf530e309eb9fe247fe4b1676c5
ValyaDudchak <48631216+ValyaDudchak@users.noreply.github.com>
2024-04-09 18:01:16 +0300

    fixed modules

--- todo-list-app/main.js
+++ todo-list-app/main.js
@@ -61,8 +61,8 @@ export function changeTaskStatus(event)
         event.currentTarget.parentNode.parentNode.prepend(event.currentTarget);
         event.currentTarget.classList.remove('ellipse-image-done');
         
-        event.currentTarget.nextSibling.firstChild.remove();
-        event.currentTarget.nextSibling.remove();
+        event.currentTarget.nextElementSibling.firstChild.remove();
+        event.currentTarget.nextElementSibling.remove();
     }
     else 
     {
@@ -82,92 +82,3 @@ export function changeTaskStatus(event)
         imgContainer.append(grayEllipse);
     }
 }
-
-// const list = [
-//     {name: "create a new practice task", status: "In Progress", priority: "low"},
-//     {name: "make a bed", status: "Done", priority: "low"},
-//     {name: "write a post", status: "To Do", priority: "high"},
-// ]
-
-// list.changeStatus = function(taskName, newStatus) {
-//     let task = this.find((item) => item.name == taskName);
-//     this[this.indexOf(task)].status = newStatus;
-// }
-
-// list.addTask = function(taskName, taskStatus = "To Do", taskPriority = "low") {
-//     this.push({name: taskName, status: taskStatus, priority: taskPriority});
-// }
-
-// list.deleteTask = function(taskName) {
-//     let task = this.find((item) => item.name == taskName);
-//     this.splice(this.indexOf(task), 1);
-// }
-
-// list.showList = function() {
-//     for(let el of this)
-//     {
-//         console.log(`- ${el.name}, status: ${el.status}`);
-//     }
-// }
-
-// list.showBy = function(grouping) {
-    
-//     if(grouping == "status")
-//     {
-//         let arr = [[], [], []];
-//         for(let el of this)
-//         {
-//             if(el.status == "To Do") arr[0].push(el);
-//             else if (el.status == "In Progress") arr[1].push(el);
-//             else if (el.status == "Done") arr[2].push(el);
-//         }
-//         for (let i = 0; i < arr.length; i++)
-//         {
-//             if(i == 0) console.log("To Do:");
-//             else if(i == 1) console.log("In Progress:");
-//             else console.log("Done:");
-            
-//             for (let j = 0; j < arr[i].length; j++) console.log(`- ${arr[i][j].name}`);
-//         }
-//     }
-    
-//     else if(grouping == "priority")
-//     {
-//         let arr = [[], []];
-//         for(let el of this)
-//         {
-//             if(el.priority == "low") arr[0].push(el);
-//             else if (el.priority == "high") arr[1].push(el);
-//         }
-//         for (let i = 0; i < arr.length; i++)
-//         {
-//             if(i == 0) console.log("Low priority:");
-//             else console.log("High priority:");
-            
-//             for (let j = 0; j < arr[i].length; j++) console.log(`- ${arr[i][j].name}`);
-//         }
-//     }
-//     else console.log("invalid input");
-// }
-
-
-// list.addTask("make dinner", "To Do", "high");
-// list.addTask("play with children", "To Do", "low");
-// list.addTask("do homework", "To Do", "high");
-// list.addTask("clean the kitchen", "To Do", "low");
-// list.addTask("exercise", "To Do", "high");
-// list.addTask("do laundry", "high");
-
-// list.changeStatus("write a post", "Done");
-// list.changeStatus("do laundry", "In Progress");
-// list.changeStatus("do homework", "Done");
-// list.changeStatus("play with children", "In Progress");
-
-// list.deleteTask("write a post");
-// list.deleteTask("create a new practise task");
-
-// list.showList();
-// console.log("\n");
-// list.showBy("status");
-// console.log("\n");
-// list.showBy("priority");
--- todo-list-app/view.js
+++ todo-list-app/view.js
@@ -1,11 +1,22 @@
 import {addNewTaskHigh, addNewTaskLow, deleteTask, changeTaskStatus} from './main.js';
 
-window.addNewTaskHigh = addNewTaskHigh;
-window.addNewTaskLow = addNewTaskLow;
-window.deleteTask = deleteTask;
-window.changeTaskStatus = changeTaskStatus;
+// window.addNewTaskHigh = addNewTaskHigh;
+// window.addNewTaskLow = addNewTaskLow;
+// window.deleteTask = deleteTask;
+// window.changeTaskStatus = changeTaskStatus;
 
-// document.querySelector('form[name="form-high"]').addEventListener('submit', addNewTaskHigh);
+document.querySelector('form[name="form-high"]').addEventListener('submit', addNewTaskHigh);
+document.querySelector('form[name="form-low"]').addEventListener('submit', addNewTaskLow);
+
+for(let el of document.querySelectorAll('.close-icon-image'))
+{
+    el.addEventListener('click', deleteTask);
+}
+
+for(let el of document.querySelectorAll('.ellipse-icon-image'))
+{
+    el.addEventListener('click', changeTaskStatus);
+}
 
 export let formHigh = document.forms["form-high"];
 export let contHigh = document.querySelector('.container-high');
