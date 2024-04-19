ea9e2a661d3d7b5b934630583475e9d9b8d52721
ValyaDudchak <48631216+ValyaDudchak@users.noreply.github.com>
2024-04-19 21:50:44 +0300

    feature: favorite cities

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,17 +1,27 @@
-import { form, currentCity, tempNow, BUTTONS, TABS } from "./view.js";
+import { UI_ELEMENTS, favoriteCities } from "./view.js";
 
 // showTabDetails();
 
-export function chooseNewCity(event) 
+export function inputNewCity(event)
 {
-    const city = form.input.value;
+    let city = UI_ELEMENTS.FORM.input.value;
     event.preventDefault();
-    form.input.value = "";
-    form.input.blur();
+    UI_ELEMENTS.FORM.input.value = "";
+    UI_ELEMENTS.FORM.input.blur();
 
+    chooseNewCity(city);
+}
+
+function calculateTemperature(temp_k)
+{
+    return Math.round(temp_k - 273.15);
+}
+
+function chooseNewCity(city)
+{
     const serverUrl1 = 'http://api.openweathermap.org/data/2.5/weather';
     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-    const url = `${serverUrl1}?q=${city}&appid=${apiKey}`;
+    let url = `${serverUrl1}?q=${city}&appid=${apiKey}`;
 
     fetch(url)
     .then(response => {
@@ -22,65 +32,110 @@ export function chooseNewCity(event)
     })
     // .then(json => console.log(json));
     .then(json => {
-        currentCity.textContent = json.name;
-        tempNow.textContent = json.main.temp;
+        UI_ELEMENTS.TAB_NOW.CITY_NAME.textContent = json.name;
+        UI_ELEMENTS.TAB_NOW.CITY_TEMPERATURE.textContent = calculateTemperature(json.main.temp);
     })
     .catch(error => alert(error.message));
 }
 
+export function chooseCityFromFavorites(event)
+{
+    let cityName = event.currentTarget.textContent;
+    chooseNewCity(cityName);
+}
+
+export function addFavoriteCity()
+{
+    try {
+        let cityName = UI_ELEMENTS.TAB_NOW.CITY_NAME.textContent;
+        if(favoriteCities.includes(cityName)) {
+            throw new SyntaxError("City is already in the list");
+        }
+        favoriteCities.push(cityName);
+
+        let listElement = document.createElement('li');
+        let span = document.createElement('span');
+        span.textContent = cityName;
+        let img = document.createElement('img');
+        img.src = "images/close-icon.svg";
+        img.alt = "close";
+        img.classList.add('close-icon');
+        
+        listElement.append(span);
+        listElement.append(img);
+
+        UI_ELEMENTS.FAVORITE_LOCATIONS.prepend(listElement);
+    }
+    catch(error) {
+        if(error instanceof SyntaxError) {
+            alert(error.message);
+        }
+        else {
+            throw error;
+        }
+    }
+}
+
+export function deleteCityFromFavorites(event)
+{
+    let cityName = event.currentTarget.previousElementSibling.textContent;
+    favoriteCities.splice(favoriteCities.indexOf(cityName));
+    event.currentTarget.parentNode.remove();
+}
+
 export function showTabNow()
 {
-    BUTTONS.BTN_NOW.className = '';
-    BUTTONS.BTN_DETAILS.className = '';
-    BUTTONS.BTN_FORECAST.className = '';
+    UI_ELEMENTS.BUTTONS.NOW.className = '';
+    UI_ELEMENTS.BUTTONS.DETAILS.className = '';
+    UI_ELEMENTS.BUTTONS.FORECAST.className = '';
 
-    BUTTONS.BTN_NOW.classList.add('black-tab');
-    BUTTONS.BTN_DETAILS.classList.add('white-tab');
-    BUTTONS.BTN_FORECAST.classList.add('white-tab', 'border-left');
+    UI_ELEMENTS.BUTTONS.NOW.classList.add('black-tab');
+    UI_ELEMENTS.BUTTONS.DETAILS.classList.add('white-tab');
+    UI_ELEMENTS.BUTTONS.FORECAST.classList.add('white-tab', 'border-left');
 
-    TABS.NOW.classList.remove('remove-tab');
-    if(!TABS.DETAILS.classList.contains('remove-tab')) {
-        TABS.DETAILS.classList.add('remove-tab');
+    UI_ELEMENTS.TABS.NOW.classList.remove('remove-tab');
+    if(!UI_ELEMENTS.TABS.DETAILS.classList.contains('remove-tab')) {
+        UI_ELEMENTS.TABS.DETAILS.classList.add('remove-tab');
     }
     else {
-        TABS.FORECAST.classList.add('remove-tab');
+        UI_ELEMENTS.TABS.FORECAST.classList.add('remove-tab');
     }
 }
 
 export function showTabDetails()
 {
-    BUTTONS.BTN_NOW.className = '';
-    BUTTONS.BTN_DETAILS.className = '';
-    BUTTONS.BTN_FORECAST.className = '';
+    UI_ELEMENTS.BUTTONS.NOW.className = '';
+    UI_ELEMENTS.BUTTONS.DETAILS.className = '';
+    UI_ELEMENTS.BUTTONS.FORECAST.className = '';
 
-    BUTTONS.BTN_NOW.classList.add('white-tab');
-    BUTTONS.BTN_DETAILS.classList.add('black-tab');
-    BUTTONS.BTN_FORECAST.classList.add('white-tab');
+    UI_ELEMENTS.BUTTONS.NOW.classList.add('white-tab');
+    UI_ELEMENTS.BUTTONS.DETAILS.classList.add('black-tab');
+    UI_ELEMENTS.BUTTONS.FORECAST.classList.add('white-tab');
 
-    TABS.DETAILS.classList.remove('remove-tab');
-    if(!TABS.NOW.classList.contains('remove-tab')) {
-        TABS.NOW.classList.add('remove-tab');
+    UI_ELEMENTS.TABS.DETAILS.classList.remove('remove-tab');
+    if(!UI_ELEMENTS.TABS.NOW.classList.contains('remove-tab')) {
+        UI_ELEMENTS.TABS.NOW.classList.add('remove-tab');
     }
     else {
-        TABS.FORECAST.classList.add('remove-tab');
+        UI_ELEMENTS.TABS.FORECAST.classList.add('remove-tab');
     }
 }
 
 export function showTabForecast()
 {
-    BUTTONS.BTN_NOW.className = '';
-    BUTTONS.BTN_DETAILS.className = '';
-    BUTTONS.BTN_FORECAST.className = '';
+    UI_ELEMENTS.BUTTONS.NOW.className = '';
+    UI_ELEMENTS.BUTTONS.DETAILS.className = '';
+    UI_ELEMENTS.BUTTONS.FORECAST.className = '';
 
-    BUTTONS.BTN_NOW.classList.add('white-tab');
-    BUTTONS.BTN_DETAILS.classList.add('white-tab', 'border-left');
-    BUTTONS.BTN_FORECAST.classList.add('black-tab');
+    UI_ELEMENTS.BUTTONS.NOW.classList.add('white-tab');
+    UI_ELEMENTS.BUTTONS.DETAILS.classList.add('white-tab', 'border-left');
+    UI_ELEMENTS.BUTTONS.FORECAST.classList.add('black-tab');
 
-    TABS.FORECAST.classList.remove('remove-tab');
-    if(!TABS.NOW.classList.contains('remove-tab')) {
-        TABS.NOW.classList.add('remove-tab');
+    UI_ELEMENTS.TABS.FORECAST.classList.remove('remove-tab');
+    if(!UI_ELEMENTS.TABS.NOW.classList.contains('remove-tab')) {
+        UI_ELEMENTS.TABS.NOW.classList.add('remove-tab');
     }
     else {
-        TABS.DETAILS.classList.add('remove-tab');
+        UI_ELEMENTS.TABS.DETAILS.classList.add('remove-tab');
     }
 }
--- weather-app/view.js
+++ weather-app/view.js
@@ -1,23 +1,49 @@
-import {chooseNewCity, showTabNow, showTabDetails, showTabForecast} from './main.js';
+import {showTabNow, showTabDetails, showTabForecast, 
+    inputNewCity, addFavoriteCity, chooseCityFromFavorites, deleteCityFromFavorites} from './main.js';
 
-document.querySelector('form').addEventListener('submit', chooseNewCity);
+document.querySelector('form').addEventListener('submit', inputNewCity);
+document.querySelector('.heart-icon').addEventListener('click', addFavoriteCity);
 
 document.querySelector('#button-now').addEventListener('click', showTabNow);
 document.querySelector('#button-details').addEventListener('click', showTabDetails);
 document.querySelector('#button-forecast').addEventListener('click', showTabForecast);
 
-export let form = document.querySelector('form');
-export let currentCity = document.querySelector('footer').firstElementChild;
-export let tempNow = document.querySelector('.degrees-text');
-
-export const BUTTONS = {
-    BTN_NOW: document.querySelector('#button-now'),
-    BTN_DETAILS: document.querySelector('#button-details'),
-    BTN_FORECAST: document.querySelector('#button-forecast'),
+for (let el of document.querySelectorAll('.locations-list li span'))
+{
+    el.addEventListener('click', chooseCityFromFavorites);
 }
 
-export const TABS = {
-    NOW: document.querySelector('.tab-now'),
-    DETAILS: document.querySelector('.tab-details'),
-    FORECAST: document.querySelector('.tab-forecast'),
+for (let el of document.querySelectorAll('.close-icon'))
+{
+    el.addEventListener('click', deleteCityFromFavorites);
 }
+
+
+export const UI_ELEMENTS = {    
+    FORM: document.querySelector('form'),
+    FAVORITE_LOCATIONS: document.querySelector('.locations-list'),
+    BUTTONS: {
+        NOW: document.querySelector('#button-now'),
+        DETAILS: document.querySelector('#button-details'),
+        FORECAST: document.querySelector('#button-forecast'),
+    },
+    TABS: {
+        NOW: document.querySelector('.tab-now'),
+        DETAILS: document.querySelector('.tab-details'),
+        FORECAST: document.querySelector('.tab-forecast'),
+    },
+    TAB_NOW: {
+        CITY_NAME: document.querySelector('footer').firstElementChild,
+        CITY_TEMPERATURE: document.querySelector('.degrees-text'),
+        ADD_ICON: document.querySelector('.heart-icon'),
+    }
+}
+
+export let favoriteCities = [
+    "Amur",
+    "Samara",
+    "Bali",
+    "Dane",
+    "Kilo",
+    "Nur-Sultan"
+];
