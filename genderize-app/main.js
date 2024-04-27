931cf042db825652e16cd12f02ecfe5cdbe85ef8
ValyaDudchak <48631216+ValyaDudchak@users.noreply.github.com>
2024-04-27 13:58:52 +0300

    refuct

--- genderize-app/main.js
+++ genderize-app/main.js
@@ -1,26 +1,25 @@
-const serverUrl1 = 'https://api.genderize.io';
-const form = document.querySelector('form');
+const SERVER_URL = 'https://api.genderize.io';
+const FORM = document.querySelector('form'); // view.js
 
 
 function changeUrl(name)
 {
-    return `${serverUrl1}/?name=${name}`;
+    return `${SERVER_URL}/?name=${name}`;
 }
 
 function showNameGender(event)
 {
     event.preventDefault();
     
-    let name = form.input.value;
+    let name = FORM.input.value;
     let url = changeUrl(name);
 
-    form.input.value = "";
-    form.input.blur();
+    FORM.input.value = "";
+    FORM.input.blur();
 
     let promise = fetch(url);
 
-    promise.then(result => result.json())
-        .then(function(json) {
-            alert(`${name} - ${json.gender}`);
-        });
+    promise
+    .then(result => result.json())
+    .then(json => alert(`${name} - ${json.gender}`));
 }
