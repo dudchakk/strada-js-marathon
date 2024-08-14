57c65fb3dd9aec6cca39a26e70c999775a3141a1
Valentyna Dudchak <dudchak.valya@gmail.com>
2024-08-14 17:33:39 +0300

    useState

--- react/films-app/src/components/RegisterModal.js
+++ react/films-app/src/components/RegisterModal.js
@@ -1,10 +1,22 @@
 import { preventDefaultForm } from "../constants";
+import { useState } from "react";
 
 const RegisterModal = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
   const handleSubmit = (e) => {
     console.log("User is registered successfully");
-    console.log(`Username: ${e.currentTarget.username.value}`);
-    console.log(`Password: ${e.currentTarget.password.value}`);
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
 
     preventDefaultForm(e, e.currentTarget.username, e.currentTarget.password);
   }
@@ -14,8 +26,8 @@ const RegisterModal = () => {
       <div className="modal-content">
         <h2>Register Form</h2>
         <form name="register" onSubmit={handleSubmit}>
-          <input type="text" placeholder="Username" name="username" />
-          <input type="text" placeholder="Password" name="password" />
+          <input type="text" placeholder="Username" name="username" onChange={handleUsernameChange} />
+          <input type="text" placeholder="Password" name="password" onChange={handlePasswordChange} />
           <input type="submit" value="REGISTER" className="btn" />
         </form>
       </div>
